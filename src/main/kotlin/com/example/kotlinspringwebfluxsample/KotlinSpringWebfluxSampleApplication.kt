d585d616dc0e0a617418109e7020e1814e97b072
kgsong <kyunggeun.song@seavantage.com>
2022-06-03 15:17:26 +0900

    first commit

---
+++ src/main/kotlin/com/example/kotlinspringwebfluxsample/KotlinSpringWebfluxSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinspringwebfluxsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringWebfluxSampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringWebfluxSampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlinspringwebfluxsample/KotlinSpringWebfluxSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinspringwebfluxsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringWebfluxSampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
